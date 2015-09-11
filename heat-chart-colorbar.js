cfea7a4571aa916054aa1fe0c11fb057ad81c19a
bmaranville <brian.maranville@nist.gov>
2015-09-10 21:32:48 -0400

    working! with colorbar

--- heat-chart-colorbar.js
+++ heat-chart-colorbar.js
@@ -5,6 +5,7 @@ if (!d3.hasOwnProperty("id")) {
 function heatChart() {
   var debug=false;
   var margin = {top: 10, right: 10, bottom: 50, left: 50};
+  var cb_margin = {top: 10, right: 50, bottom: 50, left: 10};
   var show_grid = true;
   var show_colorbar = true;
   var colorbar_width = 120;
@@ -32,6 +33,7 @@ function heatChart() {
   var y = d3.scale.linear();
   var xAxis = d3.svg.axis();
   var yAxis = d3.svg.axis();
+  var zAxis = d3.svg.axis();
   var xAxisGrid = d3.svg.axis();
   var yAxisGrid = d3.svg.axis();
   var colormap = jet_colormap;  
@@ -53,6 +55,19 @@ function heatChart() {
     zoomed.call(this);
   }
   
+  var cb_zoomed = function() {
+    var svg = d3.select(this);
+    svg.select(".z.axis").call(zAxis);
+	  zdims.zmax = Math.max.apply(Math, z.domain());
+	  zdims.zmin = Math.min.apply(Math, z.domain());
+	  chart.redrawImage();
+  }
+  var cb_zoom = d3.behavior.zoom().on("zoom.colorbar_"+ id.toFixed(), cb_zoomed);
+  var cb_resetzoom = function() {
+    cb_zoom.translate([0,0]).scale(1);
+    cb_zoomed.call(this);
+  }
+  
   var dispatch = d3.dispatch("update", "redrawImage");
   dispatch.on("redrawImage", function() {
         _redraw_backing = true;
@@ -61,7 +76,9 @@ function heatChart() {
   
   // some private working variables
   var backing_canvas = document.createElement('canvas');
+  var colorbar_backing_canvas = document.createElement('canvas');
   var _redraw_backing = true;
+  var _redraw_colorbar = true;
   var _colormap_array = [];
 
   function chart(selection) {
@@ -74,7 +91,7 @@ function heatChart() {
         height = innerheight - margin.top - margin.bottom;
       chart.outercontainer = outercontainer;
       chart.source_data = data;
-      chart.update = function() { outercontainer.transition().call(chart); };   
+      chart.update = function() { outercontainer.transition().call(chart); chart.colorbar.update(); };   
       
       if (autoscale) {
         var new_min_max = get_min_max(data, z);
@@ -187,10 +204,92 @@ function heatChart() {
       svg.selectAll("g.x")
         .attr("transform", "translate(" + margin.left + "," + height + ")");
       svg.selectAll("g.y")
-        .attr("transform", "translate(" + margin.left + ",0)");
+        .attr("transform", "translate(" + margin.left + ",0)");    
+    });
+    selection.call(chart.colorbar);
+  }
+  
+  chart.colorbar = function(selection) {
+    selection.each(function(data) {      
+      var outercontainer = d3.select(this),
+        offset_left = 0,
+        innerwidth = colorbar_width,
+        innerheight = outercontainer.node().clientHeight,
+        width = innerwidth - cb_margin.right,
+        height = innerheight - cb_margin.top - cb_margin.bottom;
+      //colorbar.name = "colorbar";
+      chart.colorbar.outercontainer = outercontainer;
+      
+      
+      // update the z axis
+      z
+        .domain([zdims.zmin, zdims.zmax])
+        .range([height, 0]);
+        
+      zAxis
+        .scale(z)
+        .ticks(numberOfTicks)
+        .tickPadding(10)	
+        .tickSubdivide(true)	
+        .orient("right");
+        
+      var cx = d3.scale.linear()
+        .domain([0, 10])
+        .range([0, width]);
+        
+      // we will bind data to the container div, a slightly non-standard
+      // arrangement.
+      var container = d3.select(this).selectAll("div.colorbar-container").data([0]);
+     
+      cb_zoom.y(z);
+      chart.colorbar.resetzoom = cb_resetzoom;
+      chart.colorbar.zoom = cb_zoom;
+      
+      // if inner container doesn't exist, build it.
+      container.enter().append("div")
+        .attr("class", "colorbar-container")
+        .attr("width", innerwidth)
+        .attr("height", innerheight)
+        .style("display", "inline-block")
+        .style("width", innerwidth + "px")
+        .style("height", innerheight + "px");
+        
+      var colorbarCanvas = container.selectAll("canvas.colorbar").data([0]);
+      colorbarCanvas.enter().append("canvas");
+      colorbarCanvas
+          .attr("width", width)
+          .attr("height", height)
+          .attr("class", "colorbar")
+          .style("width", width + "px")
+          .style("height", height + "px")
+          .style("padding-left", offset_left + "px")
+          .style("padding-right", cb_margin.right + "px")
+          .call(drawScale);
+                
+      chart.colorbar.colorbarCanvas = colorbarCanvas;
+      
+      var svg = container.selectAll("svg.colorbar").data([0]);
+      var esvg = svg.enter()
+        .append("svg")
+          .attr("class", "colorbar");
+      esvg.append("g")
+          .attr("class", "z axis");
+    
+      svg.select(".z.axis").call(zAxis);
+      svg.call(cb_zoom)
+        .on("dblclick.zoom", null)
+        .on("dblclick.resetzoom", cb_resetzoom);
+      
+      svg.attr("width", width + cb_margin.left + cb_margin.right)
+          .attr("height", height + cb_margin.top + cb_margin.bottom);
+      
+      svg.selectAll("g.z")
+        .attr("transform", "translate(" + width + ",0)");
+        
     });
   }
-
+  chart.colorbar.update = function() { this.outercontainer.transition().call(chart.colorbar); };   
+  
   chart.colormap=function(_) {
     if (!arguments.length) return colormap;
     colormap = _;
@@ -201,6 +300,7 @@ function heatChart() {
     }
     _colormap_array[256] = d3.rgb(0,0,0);
     _colormap_array[256].a = 0;
+    _redraw_colorbar = true;
     return chart;
   };
   
@@ -276,6 +376,12 @@ function heatChart() {
   chart.y = function(_) {
     if (!arguments.length) return y;
     y = _;
+    return chart;
+  };
+  
+  chart.z = function(_) {
+    if (!arguments.length) return z;
+    z = _;
     return chart;
   };
 
@@ -388,6 +494,34 @@ function heatChart() {
     context.drawImage(ctx.canvas, sxdx.sx, sxdx.sy, sxdx.sw, sxdx.sh, sxdx.dx, sxdx.dy, sxdx.dw, sxdx.dh);
   }
   
+  // Compute the pixel colors; scaled by CSS.
+  function drawScale(canvas) {
+    var maxColorIndex = 255,
+      overflowIndex = 256,
+      context = canvas.node().getContext("2d"),
+      ctx = colorbar_backing_canvas.getContext("2d");        
+    if (_redraw_colorbar) {
+      _redraw_colorbar = false;        
+      colorbar_backing_canvas.width = 1;
+      colorbar_backing_canvas.height = 256;
+      var image = ctx.createImageData(1, 256);
+      var data = image.data;
+      for (var yp = 255, p = -1; yp >= 0; --yp) {
+        var c = _colormap_array[yp];
+        data[++p] = c.r;
+        data[++p] = c.g;
+        data[++p] = c.b;
+        data[++p] = c.a;
+      }
+      ctx.putImageData(image, 0, 0);
+    }
+     
+    context.clearRect(0,0, context.canvas.width, context.canvas.height);
+    if (context.mozImageSmoothingEnabled) context.mozImageSmoothingEnabled = false;
+    if (context.imageSmoothingEnabled) context.imageSmoothingEnabled = false;
+    context.drawImage(ctx.canvas, 0, 0, 1, 256, 0, 0, context.canvas.width, context.canvas.height);
+  }
+  
   // call after setting transform
   var make_plotdata = function(source_data, dims, clim, t) {
     // source_data is 2d array
@@ -447,319 +581,6 @@ function heatChart() {
   };
   
   return chart
-  
-}
-
-function colorbar() {
-  var margin = {top: 10, right: 50, bottom: 50, left: 10};
-  var width = 105;
-  var z = d3.scale.linear();
-    
-  var dims = {
-    xmin: 0,
-    xmax: 1,
-    ymin: 0, 
-    ymax: 1,
-    xdim: 512,
-    ydim: 512,
-    zmin: 1.0,
-    zmax: 100.0
-  }
-  // create working copy of zmax and zmin, for zooming colorbar
-  var zdims = {}
-  var id = d3.id();
-  
-  
-  var x = d3.scale.linear();
-  var y = d3.scale.linear();
-  var xAxis = d3.svg.axis();
-  var yAxis = d3.svg.axis();
-  var xAxisGrid = d3.svg.axis();
-  var yAxisGrid = d3.svg.axis();
-  var colormap = d3.scale.linear()
-    .domain([0, 31, 63, 95, 127, 159, 191, 223, 255])
-    /* Jet:
-      #00007F: dark blue
-      #0000FF: blue
-      #007FFF: azure
-      #00FFFF: cyan
-      #7FFF7F: light green
-      #FFFF00: yellow
-      #FF7F00: orange
-      #FF0000: red
-      #7F0000: dark red
-      #00000000: transparent for overflow
-    */
-    .range(["#00007F", "#0000FF","#007FFF", "#00FFFF","#7FFF7F","#FFFF00","#FF7F00","#FF0000","#7F0000"]);
-  
-  var zoomed = function() {
-    var svg = d3.select(this);
-    var container = d3.select(svg.node().parentNode);
-    var data = container.data();
-    //console.log(container);
-    svg.select(".x.axis").call(xAxis);
-    svg.select(".y.axis").call(yAxis);
-    svg.select(".grid.x").call(xAxisGrid);
-    svg.select(".grid.y").call(yAxisGrid);
-    container.select('canvas.mainplot').call(drawImage, data);
-  }
-  var zoom = d3.behavior.zoom().on("zoom.heatmap_"+ id.toFixed(), zoomed);
-  var resetzoom = function() {
-    zoom.translate([0,0]).scale(1);
-    zoomed.call(this);
-  }
-  
-  var dispatch = d3.dispatch("update", "redrawImage");
-  dispatch.on("redrawImage", function() {
-        _redraw_backing = true;
-        chart.redrawImage();
-  });
-  
-  // some private working variables
-  var backing_canvas = document.createElement('canvas');
-  var _redraw_backing = true;
-  var _colormap_array = [];
-
-  function colorbar(selection) {
-    selection.each(function(data) {
-      var offset_right = (show_colorbar) ? colorbar_width + 5 : 0;
-      var outercontainer = d3.select(this),
-        innerwidth = outercontainer.node().clientWidth - offset_right,
-        innerheight = outercontainer.node().clientHeight,
-        width = innerwidth - margin.right - margin.left,
-        height = innerheight - margin.top - margin.bottom;
-      chart.outercontainer = outercontainer;
-      chart.source_data = data;
-      chart.update = function() { outercontainer.transition().call(chart); };   
-      
-      if (autoscale) {
-        var new_min_max = get_min_max(data, z);
-        zdims.zmin = new_min_max.min;
-        zdims.zmax = new_min_max.max;
-      } else {
-        zdims.zmin = dims.zmin;
-        zdims.zmax = dims.zmax;
-      }
-      var plotdata = make_plotdata(data, dims, zdims, z);
-
-      var limits = fixAspect(aspect_ratio, dims.xmin, dims.xmax, dims.ymin, dims.ymax, width, height);
-      // Update the x-scale.
-      x
-        .domain([limits.xmin, limits.xmax])
-        .range([0, width]);
-        
-      // Update the y-scale.
-      y
-        .domain([limits.ymin, limits.ymax])
-        .range([height, 0]);
-      
-      xAxisGrid
-        .scale(x)
-        .orient("bottom")
-        .ticks(numberOfTicks)
-        .tickPadding(10)
-        .tickSize(-height, 0, 0)
-        .tickFormat("");
-        
-      yAxisGrid
-        .scale(y)
-        .ticks(numberOfTicks)
-        .tickPadding(10)	
-        .tickSubdivide(true)	
-        .orient("left")
-        .tickSize(-width, 0, 0)
-        .tickFormat("")
-      
-      xAxis
-        .scale(x)
-        .ticks(numberOfTicks)
-        .tickPadding(10)	
-        .tickSubdivide(true)	
-        .orient("bottom");
-      
-      yAxis
-        .scale(y)
-        .ticks(numberOfTicks)
-        .tickPadding(10)	
-        .tickSubdivide(true)	
-        .orient("left");
-        
-        
-      // we will bind data to the container div, a slightly non-standard
-      // arrangement.
-      var container = d3.select(this).selectAll("div.heatmap-container").data([data]);
-      
-      zoom.x(x).y(y);
-      chart.resetzoom = resetzoom;
-      chart.zoom = zoom;
-      
-      // if inner container doesn't exist, build it.
-      container.enter().append("div")
-        .attr("class", "heatmap-container")
-        .attr("width", innerwidth)
-        .attr("height", innerheight)
-        .style("display", "inline-block")
-        .style("width", innerwidth + "px")
-        .style("height", innerheight + "px");
-        
-      var mainCanvas = container.selectAll("canvas.mainplot").data([plotdata]);
-      mainCanvas.enter().append("canvas");
-      mainCanvas
-          .attr("width", width)
-          .attr("height", height)
-          .attr("class", "mainplot")
-          .style("width", width + "px")
-          .style("height", height + "px")
-          .style("padding-left", margin.left + "px")
-          .style("padding-right", margin.right + "px")
-          .call(drawImage, plotdata);
-                
-      chart.mainCanvas = mainCanvas;
-      
-      var svg = container.selectAll("svg.mainplot").data([plotdata]);
-      var esvg = svg.enter()
-        .append("svg")
-          .attr("class", "mainplot");
-      esvg.append("g")
-          .attr("class", "x axis");
-      esvg.append("g")
-          .attr("class", "y axis");
-      esvg.append("g")
-          .attr("class", "x grid");           
-      esvg.append("g")
-          .attr("class", "y grid");
-      
-      svg.select(".x.axis").call(xAxis);
-      svg.select(".y.axis").call(yAxis);
-      svg.select(".x.grid").call(xAxisGrid);
-      svg.select(".y.grid").call(yAxisGrid);
-      svg.call(zoom)
-        .on("dblclick.zoom", null)
-        .on("dblclick.resetzoom", resetzoom);
-      
-      svg.attr("width", width + margin.left + margin.right)
-          .attr("height", height + margin.top + margin.bottom);
-                
-      svg.selectAll("g.x")
-        .attr("transform", "translate(" + margin.left + "," + height + ")");
-      svg.selectAll("g.y")
-        .attr("transform", "translate(" + margin.left + ",0)");
-    });
-  }
-
-  chart.colormap=function(_) {
-    if (!arguments.length) return colormap;
-    colormap = _;
-    _colormap_array = [];
-    for (var i=0; i<256; i++) {
-        _colormap_array[i] = d3.rgb(colormap(i));
-        _colormap_array[i].a = 255;
-    }
-    _colormap_array[256] = d3.rgb(0,0,0);
-    _colormap_array[256].a = 0;
-    return chart;
-  };
-  
-  // cache the colormap:
-  chart.colormap(colormap);
-
-  chart.dispatch = dispatch;
-  colorbar.redrawScale = function() {
-    _redraw_backing = true;
-    drawScale(this.mainCanvas);
-  };
-  
-  colorbar.margin = function(_) {
-    if (!arguments.length) return margin;
-    margin = _;
-    return colorbar;
-  };
-  
-  colorbar.width = function(_) {
-    if (!arguments.length) return width;
-    width = _;
-    return colorbar;
-  }
-
-  colorbar.ztransform = function(_) {
-    if (!arguments.length) return ztransform;
-    ztransform = _;
-    z = d3.scale[ztransform]();
-    _redraw_backing = true;
-    return chart;
-  };
-  
-  chart.aspect_ratio = function(_) {
-    if (!arguments.length) return aspect_ratio;
-    aspect_ratio = _;
-    return chart;
-  };
-  
-  chart.dims = function(_) {
-    if (!arguments.length) return dims;
-    dims = _;
-    return chart;
-  };
-  
-  chart.autoscale = function(_) {
-    if (!arguments.length) return autoscale;
-    autoscale = _;
-    return chart;
-  };
-  
-  chart.width = function(_) {
-    if (!arguments.length) return width;
-    width = _;
-    return chart;
-  };
-
-  chart.height = function(_) {
-    if (!arguments.length) return height;
-    height = _;
-    return chart;
-  };
-
-  chart.x = function(_) {
-    if (!arguments.length) return x;
-    x = _;
-    return chart;
-  };
-
-  chart.y = function(_) {
-    if (!arguments.length) return y;
-    y = _;
-    return chart;
-  };
-
-  // Compute the pixel colors; scaled by CSS.
-  function drawScale(canvas) {
-    var maxColorIndex = 255,
-      overflowIndex = 256,
-      context = canvas.node().getContext("2d"),
-      ctx = backing_canvas.getContext("2d");        
-    if (_redraw_backing) {
-      _redraw_backing = false;        
-      backing_canvas.width = 1;
-      backing_canvas.height = 256;
-      var image = ctx.createImageData(1, 256);
-      var data = image.data;
-      for (var yp = 255, p = -1; yp >= 0; --yp) {
-        var c = colormap[yp];
-        data[++p] = c[0];
-        data[++p] = c[1];
-        data[++p] = c[2];
-        data[++p] = c[3];
-      }
-      ctx.putImageData(image, 0, 0);
-    }
-     
-    context.clearRect(0,0, context.canvas.width, context.canvas.height);
-    if (context.mozImageSmoothingEnabled) context.mozImageSmoothingEnabled = false;
-    if (context.imageSmoothingEnabled) context.imageSmoothingEnabled = false;
-    context.drawImage(ctx.canvas, 0, 0, 1, 256, 0, 0, context.canvas.width, context.canvas.height);
-  }
-  
-  return colorbar
   
 }
 
