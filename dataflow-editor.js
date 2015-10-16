6f05fe55cf99a2102e218a61af54fa26b54e6868
Brian Maranville <brian.maranville@nist.gov>
2015-10-16 14:07:47 -0400

    working editor, including add and remove of groups and wires

--- dataflow-editor.js
+++ dataflow-editor.js
@@ -1,3 +1,31 @@
+// Editor for graph structures (based on d3)
+//
+// Developed by Brian Ben Maranville 
+// at the National Institute of Standards and Technology,
+// Gaithersburg, Maryland, USA
+// This software is in the public domain and is not copyrighted
+//
+// can be initialized with data such as:
+//
+// data = [{
+//   modules: [
+//     {
+//       title: "Load\ndata",
+//       inputs: ["in_a"],
+//       outputs: ["out_0", "out_b"],
+//       x: 200
+//     },
+//     { 
+//       title: "background",
+//       x: 300
+//     }
+//   ],
+//   wires: [{src: "0:out_b", tgt: "1:in_0"}],
+// }];
+// 
+
+/* requires('d3.js'); */
+
 if (!d3.hasOwnProperty("id")) {
   d3.id = (function(){var a = 0; return function(){return a++}})();
 }
@@ -7,29 +35,43 @@ dataflow.editor = function(data) {
   var data = data || [];
   var svg;
   var dispatch = d3.dispatch("update");
-  dispatch.on("update", function() { 
-    var sel_update = svg.selectAll(".wire").data(function(d) {return d.wires})
-    sel_update
-      .enter().append(dataflow.wire, function(d) {return "{src: " + d.src + "," + "tgt: " + d.tgt + "}"} )
-    sel_update
-      .exit().remove();
+  dispatch.on("update", update);
+  
+  var wire_keyfn = function(d) {return "{src: " + d.src + "," + "tgt: " + d.tgt + "}"};
+  var wire_checkends = function(w) {
+    var src_exists = (w.src == 'cursor' || !(svg.select('[id=\"' + w.src + '\"]').empty()) );
+    var tgt_exists = (w.tgt == 'cursor' || !(svg.select('[id=\"' + w.tgt + '\"]').empty()) );
+    return (src_exists && tgt_exists)
+  }
+  
+  function update() {
+    var module_update = svg.selectAll(".module").data(function(d) {return d.modules}, function(d) { return d.module_id; })
+    module_update.enter().append(dataflow.module);
+    module_update.exit().remove()
+    
+    var module_ids = [];
+    module_update.each(function(d,i) {module_ids[i] = d.module_id});
+    
+    // remove wires without existing endpoints;
+    svg.datum().wires = svg.datum().wires.filter(wire_checkends);
+    
+    
+    var wire_update = svg.selectAll(".wire").data(function(d) {return d.wires}, wire_keyfn);
+    wire_update.enter().append(dataflow.wire);
+    wire_update.exit().remove();
+    
     d3.selectAll(".wire").each(draw_wire) 
-  })
+  }
   
   function get_terminal_pos(term_id) {
     var module = d3.select('[module_id="' + term_id.split(":")[0] + '"]');
     var terminal = d3.select('[id=\"' + term_id + '\"]');
     if (module.empty() || terminal.empty()) { return null }
-    
-    var terminal_tf = d3.transform(terminal.attr("transform")),
-      module_tf = d3.transform(module.attr("transform")),
-      terminal_pos = {x: terminal_tf.translate[0],
-                      y: terminal_tf.translate[1]}
-    terminal_pos.x += module_tf.translate[0];
-    terminal_pos.y += module_tf.translate[1];
-    terminal_pos.y += +terminal.attr("height")/2.0;
-    if (terminal.classed("output")) {
-      terminal_pos.x += +terminal.attr("width");
+    var reference_point = svg.node().createSVGPoint();
+    var terminal_origin = reference_point.matrixTransform(terminal.node().getCTM());
+    var terminal_pos = {
+      x: terminal_origin.x + +terminal.attr("wireoffset_x"),
+      y: terminal_origin.y + +terminal.attr("wireoffset_y")
     }
     return terminal_pos;
   }
@@ -42,19 +84,16 @@ dataflow.editor = function(data) {
     if (src == "cursor" || tgt == "cursor") {
       var mouse = d3.mouse(svg.node());
       cursor_pos = {x: mouse[0] - 3, y: mouse[1] - 3}
+      src_pos = tgt_pos = cursor_pos;
     }
-    if (src == "cursor") {
-      src_pos = cursor_pos;
-    } else {
+    if (src != "cursor") {
       src_pos = get_terminal_pos(src);
-    }
-    
-    if (tgt == "cursor") {
-      tgt_pos = cursor_pos;
-    } else {
+    }    
+    if (tgt != "cursor") {
       tgt_pos = get_terminal_pos(tgt);
     }
-    if (tgt_pos == null || src_pos == null) { 
+    if (tgt_pos == null || src_pos == null) {
+      // remove wires pointing to missing terminals
       var wires = svg.datum().wires;
         for (var i=0; i<wires.length; i++) {
           var w = wires[i]; 
@@ -63,8 +102,7 @@ dataflow.editor = function(data) {
             break;
           }
         };
-        //svg.update();
-        connector.remove() 
+        dispatch.update();
     }
     else {
       connector.attr("d", makeConnector(src_pos, tgt_pos));
@@ -87,14 +125,15 @@ dataflow.editor = function(data) {
       .enter().append("svg")
       .classed("editor", true)
       
-    svg.selectAll(".module.base")
-      .data(function(d) { return d.modules })
+    svg.selectAll(".module")
+      .data(function(d) { return d.modules }) 
       .enter().append(dataflow.module)
       
     svg.selectAll(".wire")
       .data(function(d) {return d.wires})
-      .enter().append(dataflow.wire, function(d) {return "{src: " + d.src + "," + "tgt: " + d.tgt + "}"} )
+      .enter().append(dataflow.wire)
     
+    console.log(JSON.stringify(data[0].wires));
     // bind the update function directly to the svg so children can use.
     Object.defineProperty(svg.node(), 'update', {value: dispatch.update});
       
@@ -110,6 +149,8 @@ dataflow.editor = function(data) {
   editor.svg = function() {
     return svg;
   }
+  
+  editor.update = update;
   
   return editor;
 }
@@ -191,7 +232,6 @@ dataflow.module = function(module_data) {
       .classed("highlight", false)
       .on("mouseenter", null)
       .on("mouseleave", null)
-    console.log(active_wire);
     active_wire = false;
   }
   
@@ -201,23 +241,29 @@ dataflow.module = function(module_data) {
   }
   
   // create and append module HTML element:
+  console.log('creating: ', id);
   var group = d3.select(document.createElementNS("http://www.w3.org/2000/svg", "g"))
     .datum(module_data)
-    .classed("module base", true)
+    .classed("module", true)
     .style("cursor", "move")
     .attr("transform", "translate(" + module_data.x.toFixed() + "," + module_data.y.toFixed() + ")")
     .attr("x-origin", module_data.x.toFixed())
     .attr("y-origin", module_data.y.toFixed())
     .attr("module_id", id)
     
+    // this is a bit of a hack: creating a not-so-visible read-only property of 
+    // the data object referring to the module_id, so that d3 data join will be 
+    // able to relink data and selections;
+    Object.defineProperty(module_data, "module_id", {get: function() {return id;}});
+    
     var title = group.append("g")
-      .classed("module title", true)
+      .classed("title", true)
     
     var width = 75 + (padding * 2);
     var height = 20 + padding * 2;
       
     var titleborder = title.append("rect")
-      .classed("module title border", true)
+      .classed("title border", true)
       .style("fill", "#ffffff")
       .style("stroke-width", "2px")
       .style("stroke", "#0000ff")
@@ -227,7 +273,7 @@ dataflow.module = function(module_data) {
       .attr("y", 0)
       
     var titlebox = title.append("text")
-      .classed("module title text", true)
+      .classed("title text", true)
       .text(function(d) {return d.title})
       .attr("x", padding)
       .attr("y", padding)
@@ -247,6 +293,8 @@ dataflow.module = function(module_data) {
         .style("stroke", "#0000ff")
         .attr("width", width/2)
         .attr("height", 20)
+        .attr("wireoffset_x", 0)
+        .attr("wireoffset_y", height/2)
         .attr("transform", function(d,i) { return "translate(0," + (height + i*20).toFixed() + ")"})
         .attr("id", function(d) { return id.toFixed() + ":" + d; })
         .call(wireaction)
@@ -264,8 +312,8 @@ dataflow.module = function(module_data) {
         .style("stroke", "#0000ff")
         .attr("width", width/2)
         .attr("height", 20)
-        .attr("wiring_x", width/2)
-        .attr("wiring_y", height/2)
+        .attr("wireoffset_x", width/2)
+        .attr("wireoffset_y", height/2)
         .attr("transform", function(d,i) { return "translate(" + (width/2).toFixed() + "," + (height + i*20).toFixed() + ")"})
         .attr("id", function(d) { return id.toFixed() + ":" + d; })
         .call(wireaction)
