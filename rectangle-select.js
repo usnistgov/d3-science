8b3428825afb02cf5a7f1e2b2cdfe15bdb1cd183
Brian Ben Maranville <brian.maranville@nist.gov>
2016-08-05 11:01:42 -0400

    adding callbacks to rectangle select (not very useful without)

--- rectangle-select.js
+++ rectangle-select.js
@@ -1,55 +1,3 @@
-/*
-    var drag = d3.behavior.drag();
-    svg.call(drag);
-      
-
-      drag
-        .on("dragstart.select", function() {
-          if (!selectRect) return;
-          var e = this,
-            origin = d3.mouse(e),
-            rect = d3.select(this).append("rect").attr("class", "zoom");
-          d3.select("body").classed("noselect", true);
-          origin[0] = Math.max(0, Math.min(width, origin[0]));
-          origin[1] = Math.max(0, Math.min(height, origin[1]));
-          //d3.select(window)
-          drag
-            .on("drag.zoomRect", function() {
-              var m = d3.mouse(e);
-              m[0] = Math.max(0, Math.min(width, m[0]));
-              m[1] = Math.max(0, Math.min(height, m[1]));
-              rect.attr("x", Math.min(origin[0], m[0]))
-                .attr("y", Math.min(origin[1], m[1]))
-                .attr("width", Math.abs(m[0] - origin[0]))
-                .attr("height", Math.abs(m[1] - origin[1]));
-            })
-            .on("dragend.zoomRect", function() {
-              //d3.select(window).on("mousemove.zoomRect", null).on("mouseup.zoomRect", null);
-              drag.on("drag.zoomRect", null).on("drag.zoomRect", null);
-              d3.select("body").classed("noselect", false);
-              var m = d3.mouse(e);
-              m[0] = Math.max(0, Math.min(width, m[0]));
-              m[1] = Math.max(0, Math.min(height, m[1]));
-              if (m[0] !== origin[0] && m[1] !== origin[1]) {
-                zoom.x(x.domain([origin[0], m[0]].map(x.invert).sort(function(a,b) {return a-b})))
-                    .y(y.domain([origin[1], m[1]].map(y.invert).sort(function(a,b) {return a-b})));
-              } 
-              else {
-                // reset zoom on single click? No!
-
-                //zoom.scale(1);
-                //zoom.translate([0,0]);
-                //zoom.x(x.domain([min_x, max_x]))
-                //    .y(y.domain([min_y, max_y]));
-
-              }
-              rect.remove();
-              zoomed();
-            }, true);
-          d3.event.sourceEvent.stopPropagation();
-        });
-*/
-
 function rectangleSelect(drag, x, y) {
   // x, y are d3.scale objects (linear, log, etc) from parent
   // dispatch is the d3 event dispatcher: should have event "update" register
@@ -60,51 +8,7 @@ function rectangleSelect(drag, x, y) {
   var x = x || d3.scale.linear();
   var y = y || d3.scale.linear();
   var selectRect = true;
-
-  var line = d3.svg.line()
-    .x(function(d) { return x(d[0]); })
-    .y(function(d) { return y(d[1]); });
-         
-  var state_to_pairs = function(state) {
-    // convert from xmin, xmax... to pairs of points for rectangle
-    if (show_lines) {
-      return [
-        [[state.xmin, state.ymin], [state.xmax, state.ymin]],
-        [[state.xmax, state.ymin], [state.xmax, state.ymax]],
-        [[state.xmax, state.ymax], [state.xmin, state.ymax]],
-        [[state.xmin, state.ymax], [state.xmin, state.ymin]]
-      ]
-    }
-    else {
-      return [];
-    }
-  }
-  
-  var state_to_points = function(state) {
-    if (show_points) {
-      return [
-        [state.xmin, state.ymin],
-        [state.xmax, state.ymin],
-        [state.xmax, state.ymax],
-        [state.xmin, state.ymax],
-      ]
-    }
-    else { 
-      return [];
-    }
-  }
-  
-  var state_to_center = function(state) {
-    if (show_center) {
-      return [
-        [x.invert((x(state.xmax) + x(state.xmin)) / 2.0),
-         y.invert((y(state.ymax) + y(state.ymin)) / 2.0)]
-      ]
-    }
-    else {
-      return [];
-    }
-  }
+  var callbacks = [];
     
   function interactor(selection) {
     // selection is chart mainview, need parent svg:
@@ -122,8 +26,6 @@ function rectangleSelect(drag, x, y) {
             height = Math.max.apply(Math, y.range());
         origin[0] = Math.max(0, Math.min(width, origin[0]));
         origin[1] = Math.max(0, Math.min(height, origin[1]));
-        //d3.select(window)
-        console.log("dragstart:", origin);
         drag
           .on("drag.select", function() {
             var m = d3.mouse(e);
@@ -135,26 +37,24 @@ function rectangleSelect(drag, x, y) {
               .attr("height", Math.abs(m[1] - origin[1]));
           })
           .on("dragend.select", function() {
-            //d3.select(window).on("mousemove.zoomRect", null).on("mouseup.zoomRect", null);
             drag.on("drag.select", null).on("dragend.select", null);
             d3.select("body").classed("noselect", false);
             var m = d3.mouse(e);
             m[0] = Math.max(0, Math.min(width, m[0]));
             m[1] = Math.max(0, Math.min(height, m[1]));
             if (m[0] !== origin[0] && m[1] !== origin[1]) {
-              //zoom.x(x.domain([origin[0], m[0]].map(x.invert).sort(function(a,b) {return a-b})))
-              //    .y(y.domain([origin[1], m[1]].map(y.invert).sort(function(a,b) {return a-b})));
-              console.log(([origin[0], m[0]].map(x.invert).sort(function(a,b) {return a-b})));
-              console.log(([origin[1], m[1]].map(y.invert).sort(function(a,b) {return a-b})));
+              var xdomain = ([origin[0], m[0]].map(x.invert).sort(function(a,b) {return a-b})),
+                  ydomain = ([origin[1], m[1]].map(y.invert).sort(function(a,b) {return a-b})),
+                  new_xmin = xdomain[0],
+                  new_xmax = xdomain[1],
+                  new_ymin = ydomain[0],
+                  new_ymax = ydomain[1];
+              callbacks.forEach(function(c) {
+                c(new_xmin, new_xmax, new_ymin, new_ymax);
+              });
             } 
             else {
-              // reset zoom on single click? No!
-              /*
-              zoom.scale(1);
-              zoom.translate([0,0]);
-              zoom.x(x.domain([min_x, max_x]))
-                  .y(y.domain([min_y, max_y]));
-              */
+              // do something with single click
             }
             rect.remove();
           }, true);
@@ -180,6 +80,12 @@ function rectangleSelect(drag, x, y) {
   interactor.selectRect = function(_) {
     if (!arguments.length) return selectRect;
     selectRect = _;
+    return interactor;
+  }
+  
+  interactor.callbacks = function(_) {
+    if (!arguments.length) return callbacks;
+    callbacks.push(_);
     return interactor;
   }
   
