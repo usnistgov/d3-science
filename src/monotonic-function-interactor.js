40241bc7268964938c13840f817f31e99e465b59
Brian Ben Maranville <brian.maranville@nist.gov>
2017-08-28 10:51:29 -0400

    style fix

--- src/monotonic-function-interactor.js
+++ src/monotonic-function-interactor.js
@@ -9,45 +9,54 @@ function monotonicFunctionInteractor(state, x, y) {
   // dispatch is the d3 event dispatcher: should have event "update" register
   //var state = options;
   var name = state.name;
-  var radius = state.radius == null ? 5 : state.radius;
+  var radius = (state.radius == null) ? 5 : state.radius;
   var event_name = "functional." + state.name;
   var dispatch = d3.dispatch("update");
   var x = x || d3.scaleLinear();
   var y = y || d3.scaleLinear();
-  var interpolation = state.interpolation == null ? 'Linear' : state.interpolation;
+  var interpolation = (state.interpolation == null) ? 'Linear' : state.interpolation;
 
-  var line = d3.line().x(function (d) {
-    return d;
-  }).y(function (d) {
-    return y(state.functional(x.invert(d)));
-  }).curve(d3["curve" + interpolation]);
+  var line = d3.line()
+    .x(function (d) { return d; })
+    .y(function (d) { return y(state.functional(x.invert(d))); })
+    .curve(d3["curve" + interpolation]);
 
   function interactor(selection) {
-    var group = selection.append("g").classed("interactors interactor-" + name, true).style("cursor", "auto");
-    var knot_group = group.append("g").attr("class", "knots").style("stroke", state.color1).style("stroke-linecap", "round").style("stroke-width", "4px").style("fill", "none");
-    knot_group.append("path").classed("functional", true);
-
-    interactor.update = function () {
-      var knot_x = state.show_lines == false ? [] : d3.range(0, x.range()[1], state.dx || 1);
-
+    var group = selection.append("g")
+      .classed("interactors interactor-" + name, true)
+      .style("cursor", "auto")
+    var knot_group = group.append("g")
+          .attr("class", "knots")
+          .style("stroke", state.color1)
+          .style("stroke-linecap", "round")
+          .style("stroke-width", "4px")
+          .style("fill", "none")
+    knot_group.append("path")
+      .classed("functional", true);
+      
+    interactor.update = function() {
+      var knot_x = (state.show_lines == false) ? [] : 
+        d3.range(0, x.range()[1], state.dx || 1);
+      
       var knots = knot_group.selectAll("path.functional").data([knot_x]);
-
-      knots.attr("d", line);
-      //.attr("visibility", (state.show_lines) ? "visible" : "hidden"); 
-
+        
+      knots
+        .attr("d", line)
+        //.attr("visibility", (state.show_lines) ? "visible" : "hidden"); 
+        
       // fire!
       dispatch.call("update");
-    };
+    }
     interactor.update();
   }
 
-  interactor.x = function (_) {
+  interactor.x = function(_) {
     if (!arguments.length) return x;
     x = _;
     return interactor;
   };
 
-  interactor.y = function (_) {
+  interactor.y = function(_) {
     if (!arguments.length) return y;
     y = _;
     return interactor;
