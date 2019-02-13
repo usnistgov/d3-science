c80da960e10c80868379e46551c952ebd73223f9
bbm <brian.maranville@nist.gov>
2019-02-13 11:01:41 -0500

    fix breaking cursor on NaN for z_coord

--- src/heat-chart.js
+++ src/heat-chart.js
@@ -518,7 +518,7 @@ export default function heatChart(options_override) {
           ", " + 
           y_coord.toPrecision(5) + 
           ", " + 
-          z_coord.toPrecision(5));
+          ((isNaN(z_coord)) ? 'NaN' : z_coord.toPrecision(5)));
       }
       svg
         .on("mousemove.position_cursor", follow)
