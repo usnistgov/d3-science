e73d955ddffccabacd0d58d27801d3f793a45285
bbm <brian.maranville@nist.gov>
2019-02-14 09:49:02 -0500

    updates to colormaps to include linear-visual-response set

--- make_colormaps_json.py
+++ make_colormaps_json.py
@@ -5,6 +5,7 @@ from collections import OrderedDict
 
 x = linspace(0, 1.0, 256, True) # make all 256 steps
 maps=[m for m in cm.datad if not m.endswith("_r")]
+maps += [m for m in cm.cmaps_listed if not m.endswith('_r')]
 maps.sort()
 colors_dict = OrderedDict()
 for m in maps:
@@ -39,6 +40,25 @@ footer = """
   }
 })();
 """
+
+es6_header = """\
+import * as d3 from 'd3'
+export const colormap_data = 
+"""
+es6_footer = """
+
+export const colormap_names = Object.keys(colormap_data);
+
+var x = d3.range(256);
+
+export function get_colormap(cmap_name) {
+  var cmap = d3.scaleLinear()
+    .domain(x)
+    .range(colormap_data[cmap_name])
+  return cmap
+}
+"""
+
 with open("get_colormap.js", "w") as outfile:
     outfile.write(header)
     outfile.write(json.dumps(colors_dict))
@@ -46,3 +66,8 @@ with open("get_colormap.js", "w") as outfile:
     
 with open("colormaps.json", "w") as outfile:
     outfile.write(json.dumps(colors_dict))
+
+with open("src/colormap.js", "w") as outfile:
+    outfile.write(es6_header)
+    outfile.write(json.dumps(colors_dict))
+    outfile.write(es6_footer)
