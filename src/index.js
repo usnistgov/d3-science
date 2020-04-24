ddf09adf81da397685eecd541ce571e7f82dc1ca
Brian Ben Maranville <brian.maranville@nist.gov>
2020-04-23 21:41:47 -0400

    adding package.json

---
+++ src/index.js
@@ -0,0 +1,18 @@
+export {default as heatChart} from './heat-chart.js';
+export {default as xyChart} from './xy-chart.js';
+export {extend} from './jquery-extend.js';
+export {default as rectangleSelect} from './rectangle-select.js';
+export {colormap_names, get_colormap, colormap_data} from './colormap.js';
+export {default as dataflowEditor} from './dataflow-editor.js';
+export {default as xSliceInteractor} from './x-slice-interactor.js';
+export {default as rectangleInteractor} from './rectangle-interactor.js';
+export {default as monotonicFunctionInteractor} from './monotonic-function-interactor.js';
+export {default as ySliceInteractor} from './y-slice-interactor.js';
+export {default as ellipseInteractor} from './ellipse-interactor.js';
+export {default as heatChartMulti} from './heat-chart-multi.js';
+export {default as heatChartMultiMasked} from './heat-chart-multi-masked.js';
+export {linePolygonIntersects, lineLineIntersect, reflect} from './linePolygonIntersect.js';
+export {default as polygonInteractor} from './polygon-interactor.js';
+export {default as profileInteractor} from './profile-interactor.js';
+export {default as angleSliceInteractor} from './angle-slice-interactor.js';
+export {generateID} from './generate-id.js';
