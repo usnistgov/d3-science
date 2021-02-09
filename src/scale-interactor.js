f4f676a0be916451a822e5881e573f0b3a62202c
Brian Ben Maranville <brian.maranville@nist.gov>
2021-02-08 22:31:59 -0500

    update scale interactor so that it is possible to alter the scales externally, and reflect the result in the plot

--- src/scale-interactor.js
+++ src/scale-interactor.js
@@ -10,29 +10,16 @@ function scaleInteractor(state, x, y, d3_import = null) {
 
   function interactor(selection) {
     let unscaled_data = [];
-    selection.selectAll(".dot").on("click", null); // clear previous handlers
     selection.selectAll("g.series").each(function (d, i) {
-      // i is index of dataset
-      // make a copy of the data:
       unscaled_data[i] = extend(true, [], d);
-      var new_scale = state.scales[i] || 1;
-      d.forEach(function (ddd, iii) {
-        var old_point = unscaled_data[i][iii];
-        ddd[1] = new_scale * old_point[1];
-        if (ddd[2] && ddd[2].yupper != null) {
-          ddd[2].yupper = new_scale * old_point[2].yupper;
-        }
-        if (ddd[2] && ddd[2].ylower != null) {
-          ddd[2].ylower = new_scale * old_point[2].ylower;
-        }
-      });
-      var dragmove_point = function (dd, ii) {
-        var new_x = x.invert(d3.event.x),
-          new_y = y.invert(d3.event.y),
-          old_point = unscaled_data[i][ii],
-          old_x = old_point[0],
-          old_y = old_point[1];
-        new_scale = new_y / old_y;
+    });
+    selection.selectAll(".dot").on("click", null); // clear previous handlers
+
+    var update = function() {
+      selection.selectAll("g.series").each(function (d, i) {
+        // i is index of dataset
+        // make a copy of the data:
+        var new_scale = state.scales[i] || 1;
         d.forEach(function (ddd, iii) {
           var old_point = unscaled_data[i][iii];
           ddd[1] = new_scale * old_point[1];
@@ -42,10 +29,23 @@ function scaleInteractor(state, x, y, d3_import = null) {
           if (ddd[2] && ddd[2].ylower != null) {
             ddd[2].ylower = new_scale * old_point[2].ylower;
           }
-        })
+        });
+      });
+    }
+    interactor.update = update;
+
+    selection.selectAll("g.series").each(function (d, i) {
+      var dragmove_point = function (dd, ii) {
+        var new_x = x.invert(d3.event.x),
+          new_y = y.invert(d3.event.y),
+          old_point = unscaled_data[i][ii],
+          old_x = old_point[0],
+          old_y = old_point[1];
+        let new_scale = new_y / old_y;
         state.scales[i] = new_scale; // * original_datum[i];
         dispatch.call("update");
       }
+
       var drag_point = d3.drag()
         .on("drag", dragmove_point)
         .on("start", function () { d3.event.sourceEvent.stopPropagation(); dispatch.call("start") })
@@ -56,10 +56,6 @@ function scaleInteractor(state, x, y, d3_import = null) {
         .attr("r", (state.point_size || 7)) // bigger for easier drag...
         .call(drag_point);
     });
-  }
-
-  interactor.update = function() {
-    //dispatch.call("update");
   }
 
   interactor.x = function(_) {
